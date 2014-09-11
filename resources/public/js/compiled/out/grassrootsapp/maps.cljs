67fd0765d65a8b1a66c6506edd7910f3ca254e56
Lee Owen <fleeboy@gmail.com>
2014-09-11 06:32:34 +0100

    Go directions working (coords needed to be a string i.e "-52.343, -2.232") Doh! And needed clj->js for request map.

--- resources/public/js/compiled/out/grassrootsapp/maps.cljs
+++ resources/public/js/compiled/out/grassrootsapp/maps.cljs
@@ -7,26 +7,31 @@
 
 (def *map* nil)
 
+(def *dest-lat-lng* (google.maps.LatLng. "52.419 -2.219"))
+
 (def *directions-display* (google.maps.DirectionsRenderer.))
 
 (def *directions-service* (google.maps.DirectionsService.))
 
+(def *marker* (google.maps.Marker. *dest-lat-lng*))
+
 (def my-opts
   {"zoom" 14
    "mapTypeId" google.maps.MapTypeId.ROADMAP
-   "center" (google.maps.LatLng. 52.419,-2.219)})
+   "center" *dest-lat-lng*})
 
 (def request
-  {"origin" 52.3591317,-2.2705797
-   "destination" 52.419,-2.219
+  {"origin" "52.3591317 -2.2705797"
+   "destination" "52.419 -2.219"
    "travelMode" google.maps.TravelMode.DRIVING})
 
 (defn map-load []
   (let [elem (dom/getElement "map-canvas")]
     (set! *map* (google.maps.Map. elem (clj->js my-opts)))
+ ;;   (.setMap (google.maps.Marker. 52.419, -2.219) *map*)
+ ;;   (.setMap *marker* *map*)
     (.setMap *directions-display* *map*)
-    (.route *directions-service* request (fn [result status]
-                                           (println status google.maps.DirectionsStatus.OK)
+    (.route *directions-service* (clj->js request) (fn [result status]
                       (if (= status google.maps.DirectionsStatus.OK)
                              (.setDirections *directions-display* result))))))
 
--- resources/public/js/compiled/out/grassrootsapp/maps.js
+++ resources/public/js/compiled/out/grassrootsapp/maps.js
@@ -9,7 +9,23 @@ goog.require('om.core');
 goog.require('om.core');
 cljs.core.enable_console_print_BANG_.call(null);
 grassrootsapp.maps._STAR_map_STAR_ = null;
+grassrootsapp.maps._STAR_dest_lat_lng_STAR_ = (new google.maps.LatLng("52.419 -2.219"));
 grassrootsapp.maps._STAR_directions_display_STAR_ = (new google.maps.DirectionsRenderer());
 grassrootsapp.maps._STAR_directions_service_STAR_ = (new google.maps.DirectionsService());
-grassrootsapp.maps.my_opts = new cljs.core.PersistentArrayMap(null, 3, ["zoom",(14),"mapTypeId",google.maps.MapTypeId.ROADMAP,"center",(new google.maps.LatLng(52.419,-2.219))], null);
-grassrootsapp.maps.request = new cljs.core.PersistentArrayMap(null, 4, ["origin",52.3591317,-2.2705797,"destination",52.419,-2.219,"travelMode",google.maps.TravelMode.DRIVING], null);
+grassrootsapp.maps._STAR_marker_STAR_ = (new google.maps.Marker(grassrootsapp.maps._STAR_dest_lat_lng_STAR_));
+grassrootsapp.maps.my_opts = new cljs.core.PersistentArrayMap(null, 3, ["zoom",(14),"mapTypeId",google.maps.MapTypeId.ROADMAP,"center",grassrootsapp.maps._STAR_dest_lat_lng_STAR_], null);
+grassrootsapp.maps.request = new cljs.core.PersistentArrayMap(null, 3, ["origin","52.3591317 -2.2705797","destination","52.419 -2.219","travelMode",google.maps.TravelMode.DRIVING], null);
+grassrootsapp.maps.map_load = (function map_load(){var elem = goog.dom.getElement("map-canvas");grassrootsapp.maps._STAR_map_STAR_ = (new google.maps.Map(elem,cljs.core.clj__GT_js.call(null,grassrootsapp.maps.my_opts)));
+grassrootsapp.maps._STAR_directions_display_STAR_.setMap(grassrootsapp.maps._STAR_map_STAR_);
+return grassrootsapp.maps._STAR_directions_service_STAR_.route(cljs.core.clj__GT_js.call(null,grassrootsapp.maps.request),((function (elem){
+return (function (result,status){if(cljs.core._EQ_.call(null,status,google.maps.DirectionsStatus.OK))
+{return grassrootsapp.maps._STAR_directions_display_STAR_.setDirections(result);
+} else
+{return null;
+}
+});})(elem))
+);
+});
+goog.events.listen(window,"load",grassrootsapp.maps.map_load);
+
+//# sourceMappingURL=maps.js.map
